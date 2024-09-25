11799fed419f9b8fb90a9627e37ff3d34b527964
Martin Tavoda <240703@vutbr.cz>
2024-09-25 09:06:52 +0200

    led blink

--- cv01/Core/Src/main.c
+++ cv01/Core/Src/main.c
@@ -97,6 +97,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  LL_GPIO_SetOutputPin(LD2_GPIO_Port, LD2_Pin);
+	  LL_mDelay(200);
+	  LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
+	  LL_mDelay(200);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
